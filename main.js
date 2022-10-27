f016c353ecd8e0b36c6999eed535e501e9618551
saki-pg <115139489+saki-pg@users.noreply.github.com>
2022-10-27 18:24:35 +0900

    カウント訂正と、00:00:00.000表記に変更しました

--- main.js
+++ main.js
@@ -3,33 +3,41 @@ const START = document.getElementById('start');
 const STOP = document.getElementById('stop');
 const CLEAR = document.getElementById('clear');
 
-let seconds = 0;    //分を表示する変数seconds  初期値を0とする
+let seconds = 0;
 let milliseconds = 0;
 let minutes = 0;
+let hours = 0;
+
 
 
 let stopWatch = () => {
- milliseconds++;  //+1
-  if (milliseconds % 1 === 0) {   //1で割ってあまり0
+  milliseconds++;  
+ 
+  if (milliseconds === 1000 ) {   
   seconds++;  //+1
   milliseconds = 0;    //0
   }
   
-  if (seconds % 600 === 0) {
+  if (seconds === 60) {
   minutes++;
   seconds = 0; 
-  milliseconds = 0;
+  }
+  
+  if (minutes === 60) {
+  hours++;
+  minutes = 0; 
   }
 
-  STOP_WATCH.innerHTML = ('0' + minutes).slice(-1) + ':' + ('00' + seconds).slice(-3,-2) + ':' + ('0' + seconds).slice(-2,-1) + ':' + ('0' + seconds).slice(-1)
+  STOP_WATCH.innerHTML = ('00' + hours).slice(-2) + ':' + ('00' + minutes).slice(-2) + ':' + ('00' + seconds).slice(-2) + '.' + ('000' + milliseconds).slice(-3)
 }
+
 
 let interval;
 
 START.addEventListener('click' , () => {
 
 //スタート
-  interval = setInterval(stopWatch, 100);
+  interval = setInterval(stopWatch,1);
   START.disabled = true;  //無効化 
   STOP.disabled = false;  //有効化
   CLEAR.disabled = false; //有効化 
@@ -46,7 +54,8 @@ STOP.addEventListener('click' , () => {
 
 //リセット
 CLEAR.addEventListener('click' , () => {
-  STOP_WATCH.innerHTML = '0:0:0:0';
+  STOP_WATCH.innerHTML = '00:00:00.000';
+  hours = 0;
   minutes = 0;
   seconds = 0;
   milliseconds = 0;
